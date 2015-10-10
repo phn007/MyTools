6692b70ab9587500a7826c16a5270dd13449d9b7
phn007 <citb.pl@gmail.com>
2015-10-10 15:40:26 +0700

    Delete brandindex controller and views/brandindex folder

--- sitecreator/textsite/app/views/bb3-0/brandindex/catByBrand.php
+++
@@ -1,3 +0,0 @@
-<div id="cat-by-brand-container">
-	<?php if ( isset( $catByBrandList['list'] ) ) echo $catByBrandList['list'] ?>
-</div>
--- sitecreator/textsite/app/views/bb3-0/brandindex/index.php
+++
@@ -1,3 +0,0 @@
-<div id="brand-index-container">
-	<?php if ( isset( $brands['list'] ) ) echo $brands['list'] ?>
-</div>
